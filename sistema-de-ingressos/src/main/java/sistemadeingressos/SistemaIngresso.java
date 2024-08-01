8996c662fd514f66c1ee7aaca5897aaaaeb730e3
amaurineo <amaurineto8@gmail.com>
2024-07-31 22:57:30 -0300

    implementação do construtor do sistema

--- sistema-de-ingressos/src/main/java/sistemadeingressos/SistemaIngresso.java
+++ sistema-de-ingressos/src/main/java/sistemadeingressos/SistemaIngresso.java
@@ -1,4 +1,16 @@
 package sistemadeingressos;
 
+import java.util.HashMap;
+
 public class SistemaIngresso {
+    private HashMap<String,Show> shows;
+
+
+    public SistemaIngresso() {
+        this.shows = new HashMap<String, Show>();
+    }
+
+    public HashMap<String, Show> getShows() {
+        return shows;
+    }
 }
--- sistema-de-ingressos/src/test/java/sistemadeingressos/SistemaIngressoTest.java
+++ sistema-de-ingressos/src/test/java/sistemadeingressos/SistemaIngressoTest.java
@@ -1,12 +1,14 @@
 package sistemadeingressos;
 
-import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
 
 public class SistemaIngressoTest {
     @Test
     public void testCriarSistema() {
         SistemaIngresso sistema = new SistemaIngresso();
-        AssertTrue(sistema.getShows.isempty, "Não podem existir shows na hora em que o sistema é criado!");
+        assertTrue(sistema.getShows().isEmpty(), "Não podem existir shows na hora em que o sistema é criado!");
     }
 }
