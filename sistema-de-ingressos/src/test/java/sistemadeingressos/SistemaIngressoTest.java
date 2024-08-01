4fa0aee365507770ef1c4126d2339b7ad22a0097
amaurineo <amaurineto8@gmail.com>
2024-07-31 23:17:33 -0300

    Teste de criar show no sistema, implementado

--- sistema-de-ingressos/src/test/java/sistemadeingressos/SistemaIngressoTest.java
+++ sistema-de-ingressos/src/test/java/sistemadeingressos/SistemaIngressoTest.java
@@ -11,4 +11,13 @@ public class SistemaIngressoTest {
         SistemaIngresso sistema = new SistemaIngresso();
         assertTrue(sistema.getShows().isEmpty(), "Não podem existir shows na hora em que o sistema é criado!");
     }
+
+    @Test
+    public void criarShows(){
+        SistemaIngresso sistema = new SistemaIngresso();
+        sistema.criarShow("Show1","14/09/2025","Zeza Pacotinho",24000,50000,2000,true);
+        Show show1 = new Show("Show1","14/09/2025","Zeza Pacotinho",24000,50000,2000,true);
+        assertTrue(sistema.getShows().containsValue(show1),"Houve alguma falha na criação do show");
+    }
+
 }
