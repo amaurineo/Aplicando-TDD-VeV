d2f4a63788c44b120662cd2f649719c445f2881e
amaurineo <amaurineto8@gmail.com>
2024-07-31 23:36:01 -0300

    Teste para Criar Lote para Show

--- sistema-de-ingressos/src/test/java/sistemadeingressos/SistemaIngressoTest.java
+++ sistema-de-ingressos/src/test/java/sistemadeingressos/SistemaIngressoTest.java
@@ -13,12 +13,18 @@ public class SistemaIngressoTest {
     }
 
     @Test
-    public void criarShows(){
+    public void testCriarShows(){
         SistemaIngresso sistema = new SistemaIngresso();
         sistema.criarShow("Show1","14/09/2025","Zeza Pacotinho",24000,50000,2000,true);
         assertTrue(sistema.getShows().containsKey("Show1"), "Houve alguma falha na criação do show");
     }
 
-    
+    @Test
+    public void testCriarLoteParaShow(){
+        SistemaIngresso sistema = new SistemaIngresso();
+        sistema.criarShow("Show1","14/09/2025","Zeza Pacotinho",24000,50000,2000,true);
+        sistema.criarLoteParaShow("Show1","Lote1",5000,25,20);
+        assertTrue(sistema.getShows().get("Show1").getLotes().containsKey("Lote1"));
+    }
 
 }
