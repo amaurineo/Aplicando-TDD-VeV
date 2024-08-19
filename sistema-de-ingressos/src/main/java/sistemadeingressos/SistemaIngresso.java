4e4e96355868686b3762ebfe5b1127c707c1eed5
amaurineo <amaurineto8@gmail.com>
2024-08-18 23:00:57 -0300

    Implementação de criar show para a passagem de testes

--- sistema-de-ingressos/src/main/java/sistemadeingressos/SistemaIngresso.java
+++ sistema-de-ingressos/src/main/java/sistemadeingressos/SistemaIngresso.java
@@ -10,9 +10,29 @@ public class SistemaIngresso {
         this.shows = new HashMap<String, Show>();
     }
 
-    public void criarShow(String id, String data, String nomeArtista, double cache, double despesas, double precoIngresso, boolean diaEspecial){
-        Show show = new Show(id,data,nomeArtista,cache,despesas,precoIngresso,diaEspecial);
-        this.shows.put(id,show);
+    public void criarShow(String id, String data, String nomeArtista, double cache, double despesas, double precoIngresso, boolean diaEspecial) {
+        if (cache <= 0) {
+            if (despesas <= 0 && precoIngresso <= 0) {
+                throw new IllegalArgumentException("Erro: todos os valores inválidos.");
+            } else if (despesas <= 0) {
+                throw new IllegalArgumentException("Erro: cachê e despesas inválidos.");
+            } else if (precoIngresso <= 0) {
+                throw new IllegalArgumentException("Erro: cachê e preço do ingresso inválidos.");
+            } else {
+                throw new IllegalArgumentException("Erro: cachê inválido.");
+            }
+        } else if (despesas <= 0) {
+            if (precoIngresso <= 0) {
+                throw new IllegalArgumentException("Erro: despesas e preço do ingresso inválidos.");
+            } else {
+                throw new IllegalArgumentException("Erro: despesas inválidas.");
+            }
+        } else if (precoIngresso <= 0) {
+            throw new IllegalArgumentException("Erro: preço do ingresso inválido.");
+        } else {
+            Show show = new Show(id, data, nomeArtista, cache, despesas, precoIngresso, diaEspecial);
+            this.shows.put(id, show);
+        }
     }
 
     public void criarLoteParaShow(String showID, String loteID, int quantDeIngresso, int perCentVIP, int perCentDesconto) {
